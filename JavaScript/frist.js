258d360660da9bd912c0ae6bd88e3811b19b5426
Swapnil Thomare <swapnil2001thomare@gmail.com>
2023-03-04 23:48:00 +0530

    second function expression

--- JavaScript/frist.js
+++ JavaScript/frist.js
@@ -1,5 +1,7 @@
 /*function expression*/
 
+/* Named function declearation*/
+
 var factorial = function fact(n){
     var ans = 1;
     for(var i = 1; i <= n; i++){
@@ -9,3 +11,15 @@ var factorial = function fact(n){
   }
   
   console.log(factorial(5))
+
+  /*ananymos function declearation */
+
+  var factorial2 = function (n){
+    var ans = 1;
+    for(var i = 1; i <= n; i++){
+      ans = ans * i;
+    }
+    return ans;
+  }
+  
+  console.log(factorial2(5))
