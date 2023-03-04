f79b75d7770a1c5a342952040a48b4e2066c6cf1
Swapnil Thomare <swapnil2001thomare@gmail.com>
2023-03-05 00:26:03 +0530

    different methods in array

--- JavaScript/frist.js
+++ JavaScript/frist.js
@@ -23,3 +23,34 @@ var factorial = function fact(n){
   }
   
   console.log(factorial2(5))
+
+
+  /* Array*/
+
+var arr = [1,2,3,4];
+var arr2 = new Array(5,4,3,2,1);
+
+var arr3 = new Array(2);
+
+console.log(arr);
+console.log(arr2);
+
+
+
+
+
+
+var arr = [2,3,4,5,6,7];
+
+for(var i = 0; i <= arr.length; i++){
+  console.log(arr[i])
+}
+
+
+var arr = [2,3,4,5,6,7];
+
+function print(element){
+  console.log(element);
+}
+
+arr.forEach(print);
